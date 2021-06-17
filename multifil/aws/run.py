edda162d4280d6688da45f217a81354ef80aa9da
travistune3 <80426231+travistune3@users.noreply.github.com>
2021-06-17 13:54:53 -0500

    Update run.py

--- multifil/aws/run.py
+++ multifil/aws/run.py
@@ -487,7 +487,14 @@ class s3:
         # Download key
         downloaded_name = local + '/' + file_name
         pdb.set_trace()
-        key.get_contents_to_filename(downloaded_name)
+        
+        with open(downloaded_name, 'wb') as data:
+            key.download_fileobj(data)
+                
+            
+        pdb.set_trace()
+        
+        # key.get_contents_to_filename(downloaded_name)
         if key.size != os.stat(downloaded_name).st_size:
             print("Size mismatch, downloading again for %s: " % downloaded_name)
             key.get_contents_to_filename(downloaded_name)
