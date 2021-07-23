e28b022de3617a35a8d99228da10de9182e90c33
travistune3 <80426231+travistune3@users.noreply.github.com>
2021-07-23 16:46:41 -0500

    Update __init__.py

--- multifil/aws/__init__.py
+++ multifil/aws/__init__.py
@@ -3,5 +3,5 @@ from multifil.aws.metas import emit
 
 from multifil.utilities import use_aws
 if use_aws:
-    from .instance import queue_eater
-    from .cluster import watch_cluster
+    # from .instance import queue_eater
+    # from .cluster import watch_cluster
